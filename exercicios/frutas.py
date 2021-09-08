4087476275058407bc28b3bea66b0d2b0bc29f43
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-08 14:36:48 -0400

    Listas de frutas

--- exercicios/frutas.py
+++ exercicios/frutas.py
@@ -1,5 +1,15 @@
 
 print("*************************************")
-    print("Frutas!")
-    print("**************************************")
+print("****Frutas!*****")
+print("**************************************")
+
+frutas = ['Banana', 'Maça', 'Pera', 'Uva', 'Melancia', 'Jamelão']
+#print(frutas)
+fruta_pedida = input('Qual é a fruta que deseja consultar ?')
+
+if(fruta_pedida in frutas):
+    print('Sim, temos a fruta.')
+else:
+    print('Não temos a fruta.')
+
 
