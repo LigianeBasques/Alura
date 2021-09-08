d1cd2f1438ab4b8c14c972ffc4ac39b4df3f846c
Ligiane Basques <ligianealzie25@gmail.com>
2021-09-08 13:44:03 -0400

    o grande campeão

---
+++ exercicios/time.py
@@ -0,0 +1,9 @@
+
+colocacao = ['Drible da Emoção', 'Bruxos como Ronaldinho', 'Só golaço', '3x1 não é goleada']
+print(colocacao)
+
+
+campeao = colocacao[1]
+
+print(' O grande campeão do torneio é o time ' + campeao)
+#Resultado:  O grande campeão do torneio é o time Bruxos como Ronaldinho
